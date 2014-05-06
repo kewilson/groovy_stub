0e29fd9cef8f25ed2190657f038e6db690c74fde
unknown <kwilson@PLYWS12167.DataRecognitionCorp.com>
2014-05-06 10:25:06 -0500

    Initial commit

---
+++ src/main.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created by kwilson on 4/25/2014.
+ */
+class main {
+
+    void hello(){
+        println("Hello World!")
+    }
+}
---
+++ src/mainscr.groovy
@@ -0,0 +1,6 @@
+/**
+ * Created by kwilson on 5/6/2014.
+ */
+
+app = new main()
+app.hello()
